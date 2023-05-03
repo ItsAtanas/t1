7a1fa2fd52efbf66983b56e8f73693fa9e27ad62
Atanas Bogdanoski <bogdanoskiatanas@gmail.com>
2023-05-02 23:44:06 -0400

    pog

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>t1</title>
+        <meta name="description" content="t1 description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://atanasb.me/img/1662670355918.jpeg"
+        />
+      </Head>
+      <main>
+       <p>t1</p>
+      </main>
+    </>
+  );
+}
